8875a57d051190537e7adff9a22ba205729fcc0f
Nando123abc <frodr116@fiu.edu>
2022-01-27 18:22:16 -0500

    add

---
+++ movieapi.js
@@ -0,0 +1,11 @@
+let apikey = '37093d94a7918a4064e5b65bc9524875';
+let requestURL = 'https://api.themoviedb.org/3/movie/550?api_key='+apikey;
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  const superHeroes = request.response;
+  console.log(superHeroes);
+}
