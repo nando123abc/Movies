e944877fa26bf36337c4b164cf58c49ad5099975
Nando123abc <frodr116@fiu.edu>
2022-01-27 18:28:09 -0500

    add

--- movieapi.js
+++ movieapi.js
@@ -4,6 +4,10 @@ let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
+let response = await fetch('https://api.themoviedb.org/3/movie/550?api_key='+apikey);
+let data = await response.json();
+
+console.log(data);
 
 request.onload = function() {
   const superHeroes = request.response;
