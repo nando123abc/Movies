48cc6e45298bde474b098d2870a7b2cf09c0e9d2
Nando123abc <frodr116@fiu.edu>
2022-01-27 21:51:17 -0500

    add

--- movieapi.js
+++ movieapi.js
@@ -2,9 +2,7 @@ console.log('Hello');
 sendAPIRequest();
 async function sendAPIRequest(){
 let apikey = '37093d94a7918a4064e5b65bc9524875';
-let requestURL = 'https://api.themoviedb.org/3/movie/3116?api_key='+apikey;
-
-let response = await fetch('https://api.themoviedb.org/3/movie/550?api_key='+apikey);
+let response = await fetch('https://api.themoviedb.org/3116/movie/550?api_key='+apikey);
 let data = await response.json();
 
 ApiLoaded(data);
@@ -13,5 +11,15 @@ ApiLoaded(data);
 function ApiLoaded(data){
   
   document.querySelector("#MainTable").innerHTML += data.original_title;
+  
+  var row = document.createElement('tr');
+  row.setAttribute('class', 'row');
+  row.setAttribute('id', 'row');
+  document.querySelector("#MainTable").appendChild(row);
+  document.querySelector("#row").innerHTML += "<th> "+data.original_title+"</th>";
+  document.querySelector("#row").innerHTML += "<th> "+data.release_date+"</th>";
+  document.querySelector("#row").innerHTML += "<th> "+data.overview+"</th>";
+  document.querySelector("#row").innerHTML += "<th> "+data.vote_average+"</th>";
+  document.querySelector("#row").innerHTML += "<th> <img src=https://image.tmdb.org/t/p/w500"+data.poster_path+" alt="+data.original_title+"></th>";
   console.log(data);
 }
